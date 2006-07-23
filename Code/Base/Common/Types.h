e11de14cec8cca35b0f2b45f06d82f7858eb73a9
eriQue@glow <eriquesnk@gmail.com>
2006-07-23 12:46:16 +0100

    warnings
    [git-p4: depot-paths = "//tbl/": change = 14]

--- Code/Base/Common/Types.h
+++ Code/Base/Common/Types.h
@@ -2,6 +2,10 @@
 #ifndef NEWAGE_BASE_COMMON_TYPES_H
 #define NEWAGE_BASE_COMMON_TYPES_H
 
+#if defined __psp__
+	#include <psptypes.h>
+	#define __use_system_types
+#endif
 
 typedef unsigned char bool8;
 typedef unsigned short bool16;
@@ -9,6 +13,7 @@ typedef unsigned int bool32;
 
 typedef unsigned int uint;
 
+#if !defined __use_system_types
 typedef char s8;
 typedef short s16;
 typedef int s32;
@@ -17,7 +22,7 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
-
+#endif
 
 #ifndef NULL
 #define NULL ((void*) 0)
