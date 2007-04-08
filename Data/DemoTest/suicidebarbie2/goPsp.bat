4ffd5503f4523833e0f3165be779c0adbbc97696
ReJ@gwaihir <rej@unity3d.com>
2007-04-08 12:37:08 +0100

    update
    [git-p4: depot-paths = "//tbl/": change = 462]

--- Data/DemoTest/suicidebarbie2/goPsp.bat
+++ Data/DemoTest/suicidebarbie2/goPsp.bat
@@ -1,2 +1,3 @@
 @mkdir psp
 ..\..\..\Tools\bin\ImportScene.exe psp suicidebarbie2.fbx >suicidebarbie2.fbx.out
+..\..\..\Tools\bin\ImportScene.exe psp suicidebarbie_back2.fbx >suicidebarbie_back2.fbx.out
