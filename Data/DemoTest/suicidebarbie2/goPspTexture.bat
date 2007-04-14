4a22e4cf4d2eb4a1e3e7a8fd99a6554aeff35223
eriQue@glow <eriquesnk@gmail.com>
2007-04-14 11:02:48 +0100

    added .bat for lowered texture memory
    [git-p4: depot-paths = "//tbl/": change = 489]

---
+++ Data/DemoTest/suicidebarbie2/goPspTexture.bat
@@ -0,0 +1,8 @@
+@mkdir psp
+..\..\..\Tools\bin\ImageConverter.exe -4 -noswizzle -blend 1g.tga psp\1g.mtx
+..\..\..\Tools\bin\ImageConverter.exe -4 -noswizzle -blend 2g.tga psp\2g.mtx
+..\..\..\Tools\bin\ImageConverter.exe -4 -noswizzle -blend cred1.tga psp\cred1.mtx
+..\..\..\Tools\bin\ImageConverter.exe -4 -noswizzle -blend cred2.tga psp\cred2.mtx
+..\..\..\Tools\bin\ImageConverter.exe -4 -noswizzle -blend cred3.tga psp\cred3.mtx
+..\..\..\Tools\bin\ImageConverter.exe -4 -noswizzle -blend cred4.tga psp\cred4.mtx
+..\..\..\Tools\bin\ImageConverter.exe -4 -noswizzle -blend moln.tga psp\moln.mtx
