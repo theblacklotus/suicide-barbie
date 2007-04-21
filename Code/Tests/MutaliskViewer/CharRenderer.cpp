c2203e480641485be006dbbc251b2f78367bb6cc
eriQue@glow <eriquesnk@gmail.com>
2007-04-21 20:16:21 +0100

    early (and very un-optimized) test of flocking text renderer..
    [git-p4: depot-paths = "//tbl/": change = 503]

--- Code/Tests/MutaliskViewer/CharRenderer.cpp
+++ Code/Tests/MutaliskViewer/CharRenderer.cpp
@@ -25,6 +25,8 @@ namespace mutalisk
 
 namespace
 {
+	std::vector<unsigned> random;
+
 	void prepareChars(mutalisk::RenderableMesh* mesh)
 	{
 		ASSERT(mesh);
@@ -73,15 +75,39 @@ namespace
 		Vertex* vertexData0 = (Vertex*)mesh->mAmplifiedVertexData[0];
 		Vertex* vertexData1 = (Vertex*)mesh->mAmplifiedVertexData[1];
 		size_t primCount = mesh->mBlueprint.vertexCount / 2;
+		if( random.size() < primCount)
+		{
+			srand(0);
+			random.resize(primCount);
+			for (size_t i = 0 ; i < random.size(); ++i)
+			{
+				random[i] = i;
+			}
+
+			for (size_t i = 0 ; i < random.size(); ++i)
+			{
+				size_t j = rand() % random.size();
+				unsigned x = random[i];
+				random[i] = random[j];
+				random[j] = x;
+			}
+		}
+		srand(0);
 		for (size_t j = 0; j < primCount; ++j)
 		{
 			size_t x = rand() & 0x3;
 			size_t y = rand() & 0x3;
 			float u = 0.25f * (float)x;
 			float v = 0.25f * (float)y;
+			vertexData0->normal.x = random[j];
+				float displace = vertexData0->normal.x;
+				printf("%f \n ", displace);
 			vertexData0->u = u;			vertexData0->v = v;			++vertexData0;
+			vertexData0->normal.x = random[j];
 			vertexData0->u = u + 0.25f;	vertexData0->v = v + 0.25f;	++vertexData0;
+			vertexData1->normal.x = random[j];
 			vertexData1->u = u;			vertexData1->v = v;			++vertexData1;
+			vertexData1->normal.x = random[j];
 			vertexData1->u = u + 0.25f;	vertexData1->v = v + 0.25f;	++vertexData1;
 		}
 
@@ -97,8 +123,122 @@ namespace
 
 	void renderChars(mutalisk::RenderableScene const& scene)
 	{
+		struct Vertex
+		{
+			float u, v;
+			Vec3 normal;
+			Vec3 pos;
+		};
+
+
+		static float time = 0;
+		float t = sinf(time += 0.1f);
+		if (t < 0)
+			t = 0;
+		float loop = 5.f;
+		t = fmodf(time*.6f, loop);
+		if (t > loop-0.5f)
+			t = 0.f;
+		if (t > loop-1.f)
+			t = loop-1.f;
+
+		size_t globPrimCount = 0xffffffff;
+
+		// pre-pass - transform between subspace clusters
 		const mutalisk::array<mutalisk::data::scene::Actor>& actors = scene.mBlueprint.actors;
 		mutalisk::array<mutalisk::RenderableScene::SharedResources::Mesh>& meshes = const_cast<mutalisk::RenderableScene&>(scene).mResources.meshes;
+
+	if (!false)
+	{
+		for(size_t q = 0; q < actors.size(); ++q)
+		{
+			unsigned meshIndex = actors[q].meshIndex;
+			ASSERT(meshIndex >= 0 && meshIndex < meshes.size());
+			mutalisk::RenderableMesh& mesh = *meshes[meshIndex].renderable;
+			if (mesh.mAmplifiedVertexData[mesh.mAmplifiedBufferIndex] == 0)
+				continue;
+			size_t xform0 = scene.mState.actor2XformIndex[q];
+			size_t xform1;
+
+			int meshIndex2 = -1;
+			for(size_t w = q+1; w < actors.size(); ++w)
+			{
+				unsigned meshIndex = actors[w].meshIndex;
+				ASSERT(meshIndex >= 0 && meshIndex < meshes.size());
+				mutalisk::RenderableMesh& mesh = *meshes[meshIndex].renderable;
+				if (mesh.mAmplifiedVertexData[mesh.mAmplifiedBufferIndex] == 0)
+					continue;
+				meshIndex2 = meshIndex;
+				xform1 = scene.mState.actor2XformIndex[w];
+				break;
+			}
+			if (meshIndex2 == -1)
+			{
+				for(size_t w = 0; w < actors.size(); ++w)
+				{
+					unsigned meshIndex = actors[w].meshIndex;
+					ASSERT(meshIndex >= 0 && meshIndex < meshes.size());
+					mutalisk::RenderableMesh& mesh = *meshes[meshIndex].renderable;
+					if (mesh.mAmplifiedVertexData[mesh.mAmplifiedBufferIndex] == 0)
+						continue;
+					meshIndex2 = meshIndex;
+					xform1 = scene.mState.actor2XformIndex[w];
+					break;
+				}
+			}
+			if (meshIndex == meshIndex2)
+				break;
+			ASSERT(meshIndex2 >= 0 && meshIndex2 < meshes.size());
+			mutalisk::RenderableMesh& mesh2 = *meshes[meshIndex2].renderable;
+
+//			printf("%i > %i = %i > %i\n", meshIndex, meshIndex2, mesh.mBlueprint.vertexCount, mesh2.mBlueprint.vertexCount);
+
+
+			Vertex* vertexData0 = (Vertex*)mesh.mAmplifiedVertexData[mesh.mAmplifiedBufferIndex];
+			Vertex* vertexData1 = (Vertex*)mesh2.mAmplifiedVertexData[mesh2.mAmplifiedBufferIndex];
+			size_t primCount = std::min(mesh.mBlueprint.vertexCount, mesh2.mBlueprint.vertexCount) / 2;
+			globPrimCount = std::min(globPrimCount, primCount);
+			Vec3 pivot0 = vertexData0->pos;
+			Vec3 pivot1 = vertexData1->pos;
+
+			for (size_t j = 0; j < primCount; ++j)
+			{
+				Vec3 pos0 = vertexData0->pos;
+				Vec3 pos1 = vertexData1->pos;
+				Vec3 pos3, pivot3;
+				float displace = random[j];
+//				printf("%f \n ", displace);
+				float td = t - 0.5f * (displace/(float)primCount);
+				td = std::max(td, 0.f);
+				td = std::min(td, 1.f);
+//				printf("%f -> ", td);
+				td = sinf(((td-0.5f) * 3.1415926535f)) * 0.5f + 0.5f;
+//				printf("%f \n", td);
+				Vec3_sub(&pos3, &pos1, &pos0);
+				Vec3_scale(&pos3, &pos3, td);
+				Vec3_add(&pos3, &pos3, &pos0);
+
+				Vec3_sub(&pivot3, &pivot1, &pivot0);
+				Vec3_scale(&pivot3, &pivot3, td);
+				Vec3_add(&pivot3, &pivot3, &pivot0);
+
+				Vec3_sub(&pivot3, &pivot3, &pos3);
+				Vec3_scale(&pivot3, &pivot3, sinf(td*3.1415956535f) * 0.75f);
+				Vec3_add(&pos3, &pivot3, &pos3);
+
+
+				++vertexData0;
+				vertexData0->pos = pos3; ++vertexData0;
+				++vertexData1;
+				++vertexData1;
+			}
+
+		}
+
+	}
+//		printf("globPrimCount = %i\n", globPrimCount);
+
+
 		for(size_t q = 0; q < actors.size(); ++q)
 		{
 			unsigned meshIndex = actors[q].meshIndex;
@@ -114,22 +254,20 @@ namespace
 			Vec3 vec;
 			Vec3_setMat33MulVec3(&vec, &cw, &unit2d);
 
-			struct Vertex
-			{
-				float u, v;
-				Vec3 normal;
-				Vec3 pos;
-			};
-
 			Vertex* vertexData = (Vertex*)mesh.mAmplifiedVertexData[mesh.mAmplifiedBufferIndex];
 			size_t primCount = mesh.mBlueprint.vertexCount / 2;
 			Vec3 p0, p1, v;
 			Vec3_scale(&v, &vec, 0.03f);
 			for (size_t j = 0; j < primCount; ++j)
 			{
-				Vec3 pos = vertexData->pos;
+				Vec3 pos = (vertexData+1)->pos;
 				Vec3_sub(&p0, &pos, &v);
 				Vec3_add(&p1, &pos, &v);
+				if (j >= globPrimCount /*|| (j & 0x1)*/)
+				{
+					Vec3_scale(&p0, &p0, 0);
+					Vec3_scale(&p1, &p1, 0);
+				}
 				vertexData->pos = p0; ++vertexData;
 				vertexData->pos = p1; ++vertexData;
 			}
