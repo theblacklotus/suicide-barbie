6657f59fd245796cc5df28a5167d3e863a016d43
eriQue@glow <eriquesnk@gmail.com>
2007-05-10 00:18:56 +0100

    added missing scenes
    [git-p4: depot-paths = "//tbl/": change = 583]

--- Data/DemoTest/goPsp.bat
+++ Data/DemoTest/goPsp.bat
@@ -2,6 +2,7 @@
 @call buildScenePsp.bat logo
 @call buildScenePsp.bat flower
 @call buildScenePsp.bat head
+@call buildScenePsp.bat Snake
 
 @call buildScenePsp.bat telephone_s1
 @call buildScenePsp.bat telephone_s2
@@ -10,17 +11,18 @@
 @call buildScenePsp.bat telephone_trans
 
 @call buildScenePsp.bat text
-@call buildScenePsp.bat back_01
-@call buildScenePsp.bat back_02
 @call buildScenePsp.bat jealousy
 
-rem @call buildScenePsp.bat mix/mix1
-rem @call buildScenePsp.bat mix/mix2
-rem @call buildScenePsp.bat mix/mix3
-rem @call buildScenePsp.bat mix/garlic1
-rem @call buildScenePsp.bat mix/garlic2
+@call buildScenePsp.bat beer/beer1
+@call buildScenePsp.bat beer/beer2
+@call buildScenePsp.bat mix/mix1
+@call buildScenePsp.bat mix/mix2
+@call buildScenePsp.bat mix/mix3
+@call buildScenePsp.bat garlic/garlic1
+@call buildScenePsp.bat garlic/garlic2
 
 @call buildScenePsp.bat reload
+@call buildScenePsp.bat weapon2
 @call buildScenePsp.bat weapon3
 @call buildScenePsp.bat bull1
 @call buildScenePsp.bat bull2
@@ -31,4 +33,5 @@ rem @call buildScenePsp.bat mix/garlic2
 @call buildScenePsp.bat exgirl2
 
 @call buildScenePsp.bat suicidebarbie1
+@call buildScenePsp.bat suicidebarbie2
 @call buildScenePsp.bat window
